4ace5d8de36147d1c3392a628a4d576afb59b1e7
neubrom <epro050@t-online.de>
2021-08-24 23:10:41 +0200

    Day 03

---
+++ 03/day-3-1-exercise/main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+check = number%2
+if check == 0:
+    print(f"The number {number} is even")
+else:
+    print(f"The number {number} is odd")
+
+
+
