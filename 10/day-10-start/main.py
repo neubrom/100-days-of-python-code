e89447b67966c7fb3aaf0318de03a735f367425d
neubrom <epro050@t-online.de>
2021-08-30 21:44:09 +0200

    Day 10

--- 10/day-10-start/main.py
+++ 10/day-10-start/main.py
@@ -1,7 +1,5 @@
 def is_leap(year):
-  if year == None:
-    return print("wrong input")
-  elif year % 4 == 0:
+  if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
         return True
@@ -13,8 +11,6 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
-  if year == "" or month == "":
-    return print("wrong input month or year")
   if is_leap(year) == False:
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     return month_days[month-1]
