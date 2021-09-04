5b6944b45d78dca0611950dfd5e4494b2eafa267
neubrom <epro050@t-online.de>
2021-09-04 23:10:37 +0200

    Day 15

--- 15/coffee-machine-start/main.py
+++ 15/coffee-machine-start/main.py
@@ -30,22 +30,38 @@ resources = {
     "coffee": 100,
 }
 
+drink = ""
+mashine_state = True
+resources_now = resources
+
 # TODO Ask wat would the customer like 
-"""Prompt user by asking “​ What would you like? (espresso/latte/cappuccino):​
-”
+"""Prompt user by asking “​ What would you like? (espresso/latte/cappuccino):​”
 a. Check the user’s input to decide what to do next.
 b. The prompt should show every time action has completed, e.g. once the drink is
 dispensed. The prompt should show again to serve the next customer."""
 
-print("Prompt user by asking “​ What would you like? (espresso/latte/cappuccino):​")
 
-#TODO Turn off the Maschine
-"""Turn off the Coffee Machine by entering “​ off​
-” to the prompt.
+def order():
+    drink_order = input("​ What would you like? (espresso/latte/cappuccino):​")
+    if drink_order not in ("espresso", "latte", "cappuccino", "off", "report"):
+        print("Wrong input, order again!")
+        order()
+    return drink_order
+
+
+# TODO Turn off the Maschine
+"""Turn off the Coffee Machine by entering “​ off​” to the prompt.
 a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
 the machine. Your code should end execution when this happens."""
 
-#TODO Print report.
+
+def mashine_on_off():
+    if drink in ("espresso", "latte", "cappuccino"):
+        return True
+    elif drink == "off":
+        return False
+
+# TODO Print report.
 """When the user enters “report” to the prompt, a report should be generated that shows
 the current resource values. e.g.
 Water: 100ml
@@ -53,7 +69,13 @@ Milk: 50ml
 Coffee: 76g
 Money: $2.5"""
 
-#TODO Check resources sufficient?
+
+def report(drink_order):
+    if drink_order == "report":
+        print(resources_now)
+
+
+# TODO Check resources sufficient?
 """a. When the user chooses a drink, the program should check if there are enough
 resources to make that drink.
 b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
@@ -61,14 +83,17 @@ not continue to make the drink but print: “​ Sorry there is not enough water
 ”
 c. The same should happen if another resource is depleted, e.g. milk or coffee."""
 
-#TODO Process coins.
+def chesk_resources(drink_order, resources_now_check):
+    # compare MENU vs resources
+
+# TODO Process coins.
 """a. If there are sufficient resources to make the drink selected, then the program should
 prompt the user to insert coins.
 b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
 c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
 pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52"""
 
-#TODO Check transaction successful?
+# TODO Check transaction successful?
 """a. Check that the user has inserted enough money to purchase the drink they selected.
 E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
 program should say “​ Sorry that's not enough money. Money refunded.​
@@ -79,10 +104,11 @@ Water: 100ml
 Milk: 50ml
 Coffee: 76g
 Money: $2.5
-c. If the user has inserted too much money, the machine should offer change.E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
+c. If the user has inserted too much money, the machine should offer change.E.g. “Here is $2.45 dollars in change.” 
+The change should be rounded to 2 decimal
 places."""
 
-#TODO Make Coffee.
+# TODO Make Coffee.
 """a. If the transaction is successful and there are enough resources to make the drink the
 user selected, then the ingredients to make the drink should be deducted from the
 coffee machine resources.
@@ -98,3 +124,8 @@ Coffee: 76g
 Money: $2.5
 b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
 latte was their choice of drink."""
+
+
+order()
+mashine_on_off()
+report()
