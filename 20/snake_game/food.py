c233f8b76c33f9fa01e0f143e95abb7b37e124ba
neubrom <epro050@t-online.de>
2021-09-13 00:33:10 +0200

    Day 21

--- 20/snake_game/food.py
+++ 20/snake_game/food.py
@@ -6,7 +6,7 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=0.7, stretch_wid=0.7)
         self.color("red")
         self.speed("fastest")
         self.refresh()
--- 20/snake_game/main.py
+++ 20/snake_game/main.py
@@ -30,13 +30,18 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.refresh()
+        snake.extend()
         scoreboard.write(f"Score: {scoreboard.score}", True, align="center", font=("Arial", 24, "normal"))
 
     #Detect collision with wall
-    elif snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
         scoreboard.game_over()
         game_is_on = False
 
-
+    #Detect collision with tall.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
