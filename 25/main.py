2e3e0c489b76684e4ee73313d2bd8a28d9bc5d36
neubrom <epro050@t-online.de>
2021-09-20 23:04:46 +0200

    Day 25

--- 25/main.py
+++ 25/main.py
@@ -12,5 +12,16 @@
 import numpy
 import pandas as pd
 
-data = pd.read_csv("weather_data.csv")
+# data = pd.read_csv("weather_data.csv")
+# print(data)
+# df = pd.DataFrame(data)
+# mean_df_mean = round(df['temp'].mean(), 2)
+# mean_df_max = round(df['temp'].max(), 2)
+# print("Mean temp:" + f"{mean_df_mean}")
+# print("Max temp:" + f"{mean_df_max}")
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 #print(data)
+df = pd.DataFrame(data, columns=['grey', 'red', 'black'])
+df_color = df["Primary Fur Color"].count()
+print(df_color)
