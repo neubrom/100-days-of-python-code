bcd5d7b2bff83ae0f59dc0c2c1a82dc463af734a
neubrom <epro050@t-online.de>
2021-09-30 20:22:18 +0200

    Day 29

--- 29/password-manager-start/main.py
+++ 29/password-manager-start/main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+import math
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password manager")
+window.config(padx=20, pady=20, bg="white")
+
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 112, image=logo_img)
+canvas.grid(column=1, row=1)
+
+window.mainloop()
