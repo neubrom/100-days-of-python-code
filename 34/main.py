87651e2a755e9b35a6cf45cea8063370276bc7b9
neubrom <epro050@t-online.de>
2022-01-27 21:50:14 +0100

    Day 34

--- 34/main.py
+++ 34/main.py
@@ -14,7 +14,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz_ua = QuizInterface()
+quiz_ua = QuizInterface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
--- 34/ui.py
+++ 34/ui.py
@@ -1,6 +1,8 @@
+import tkinter
 from tkinter import *
 import pandas
 import random
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 BACKGROUND_COLOR = "#B1DDC6"
@@ -8,27 +10,41 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizler")
-        self.window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-        self.canvas = Canvas(width=800, height=526)
+        self.window.minsize(width=100, height=200)
 
-        self.card_background = self.canvas.create_image(400, 263, image=card_front_img)
-        self.card_title = self.canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-        self.card_word = self.canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
-        self.canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-        self.canvas.grid(row=0, column=0, columnspan=2)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.score_label = Label(text="Score:0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
 
-        self.cross_image = PhotoImage(file="images/wrong.png")
-        self.unknown_button = Button(image=self.cross_image, highlightthickness=0, command=next_card)
-        self.unknown_button.grid(row=1, column=0)
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Question here",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        self.check_image = PhotoImage(file="images/right.png")
-        self.known_button = Button(image=self.check_image, highlightthickness=0, command=is_known)
-        self.known_button.grid(row=1, column=1)
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
 
         self.window.mainloop()
 
-
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
 
